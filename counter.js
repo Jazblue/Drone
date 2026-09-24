6318223284918046e681fbaa308f930f51a2f2b0
Jason H <jazblue@users.noreply.github.com>
2026-09-24 22:21:14 +0100

    Add visitor counter

---
+++ counter.js
@@ -0,0 +1,10 @@
+ocument.addEventListener("DOMContentLoaded", function () {
+    fetch("https://countapi.mileshilliard.com/api/v1/hit/Drone")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("visitor-count").textContent = data.value;
+        })
+        .catch(() => {
+            document.getElementById("visitor-count").textContent = "unavailable";
+        });
+});
